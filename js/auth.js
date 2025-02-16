a45dc1413c6897861312d7d14f0209d304a582f3
KentXStuuki <mequiambao@outlook.com>
2025-02-16 22:51:24 +0800

    Tried to code the auth firebase

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,8 @@
 //Sign up 
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    // get user info
+    const email = signupForm['']
+})
