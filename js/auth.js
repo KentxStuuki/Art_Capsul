4a0c6c2f05b1e6d93fb14b066ff00b35e4282cba
KentXStuuki <mequiambao@outlook.com>
2026-04-19 13:52:40 +0800

    Reverted repository to previous state by restoring old files and deleting newer changes

--- js/auth.js
+++
@@ -1,8 +0,0 @@
-//Sign up 
-const signupForm = document.querySelector("#signup-form");
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    // get user info
-    const email = signupForm['']
-})
---
+++ scripts/buttons.js
@@ -0,0 +1,51 @@
+/*
+    Name: Mark Erolle D. Quiambao
+		Section: WD-201
+		Website Name: Art Capsul
+		Started: November 13,2023
+		Date of Submission:  NOvember 17, 2023
+*/
+
+/* Darkmode feature */
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+/* Next Buttons */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+/* Hamburger Menu */
+function showSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'flex'
+}
+function hideSidebar(){
+  const sidebar = document.querySelector('.sidebar')
+  sidebar.style.display = 'none'
+}
